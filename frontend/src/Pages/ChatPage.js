5dd9ed98b687e1506c85d8767e246b3ea2b3a8b4
Chakradhar <chakradhars2003s@gmail.com>
2023-06-26 01:05:02 +0530

    added

---
+++ frontend/src/Pages/ChatPage.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+const ChatPage = () => {
+    // const [chats, setChats] = useState([])
+    // const fetchChats = async () => {
+    //     const { data } = await axios.get("http://localhost:5000/api/chat");
+    //     setChats(data);
+    // }
+    // useEffect(() => {
+    //     fetchChats();
+    // }, [])
+    //{chats.map((chat) => <div key={chat.id}>{chat.chatName}</div>)}
+    return <div> </div>
+}
+
+export default ChatPage
+//nppm i axios
