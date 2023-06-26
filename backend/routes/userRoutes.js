f53574f9e31423f962e50c92a644f1127812219f
DEV-ansh7 <118380070+DEV-ansh7@users.noreply.github.com>
2023-06-26 21:14:48 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
-const { registerUser, authUser } = require("../controllers/userController");
-router.route('/').post(registerUser);
+const { registerUser, authUser, allUsers } = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
+router.route('/').post(registerUser).get(protect, allUsers);
 router.post('/login', authUser);
 
 
